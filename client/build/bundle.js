cb0dd2eaca5051027e3e79f48e5ac15f4ff1a7fb
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-09-29 21:29:03 +0100

    can no add the comments to the database from the website

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -67,26 +67,29 @@
 	  this.comments = [];
 	  this.url = "http://localhost:5000/comments.json";
 	  this.getComments();
-	  this.createForm();
 	}
 	
 	EnglishCommentsView.prototype = {
 	
 	  display: function() {
+	    this.createForm();
 	    var commentSpace = document.getElementById( 'comment-space' );
 	    var list = document.createElement( 'ul' );
 	    commentSpace.appendChild( list );
 	    for( var i = 0; i < this.comments.length; i++ ) {
 	      var comment = document.createElement( 'li' );
-	      var text = this.comments[i].name + " - " + this.comments[i].comment;
+	      var text = this.comments[i].name + " - " + this.comments[i].comment_text;
 	      comment.innerText = text;
 	      list.appendChild( comment );
 	    }
 	  },
 	
 	  getComments: function() {
+	    var commentSpace = document.getElementById( 'comment-space' );
+	    commentSpace.innerText = "";
 	    var request = new XMLHttpRequest();
 	    request.open( 'GET', this.url );
+	    request.setRequestHeader("Content-Type", "application/json")
 	    request.onload = () => {
 	      if( request.status === 200 ) {
 	        var comments = JSON.parse( request.responseText );
@@ -104,10 +107,12 @@
 	    var name = document.createElement( 'input' );
 	    name.type = 'text';
 	    name.placeHolder = 'Name...';
+	    name.id = 'name';
 	
 	    var comment = document.createElement( 'input' );
 	    comment.type = 'input';
 	    comment.placeHolder = 'Please add your comment';
+	    comment.id = 'comment';
 	
 	    var button = document.createElement( 'button' );
 	    button.innerText = 'Add Comment';
@@ -117,26 +122,23 @@
 	    commentSpace.appendChild( button );
 	
 	    button.onclick = function() {
-	      this.addComment( name.value, comment.value );
-	    }.bind( this )
-	  },
-	
-	  addComment: function( name, comment ) {
-	    var request = new XMLHttpRequest()
-	    request.open( 'POST', this.url )
-	    request.onload = () => {
-	      if( request.status === 201 ) {
+	      var request = new XMLHttpRequest()
+	      request.open( 'POST', this.url )
+	      request.setRequestHeader("Content-Type", "application/json")
+	      console.log( this.url );
+	      request.onload = () => {
 	        this.getComments();
+	        console.log( "Added" );
 	      }
-	    }
-	    var data = {
-	      comment: {
-	        name: name,
-	        comment: comment
+	      var data = {
+	        comment: {
+	          name: name.value,
+	          comment_text: comment.value
+	        }
 	      }
-	    }
-	    console.log( data );
-	    request.send( JSON.stringify( data ));
+	      console.log( data );
+	      request.send( JSON.stringify( data ));
+	    }.bind( this )
 	  },
 	}
 	
