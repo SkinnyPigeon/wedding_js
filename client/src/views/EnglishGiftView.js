96e0c9231705327f1662c3086e40fbcf3fabc57b
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2017-01-04 17:38:25 +0000

    almost finished the circles

--- client/src/views/EnglishGiftView.js
+++ client/src/views/EnglishGiftView.js
@@ -69,15 +69,69 @@ EnglishGiftView.prototype = {
 
     var circleTwo = document.createElement( 'td' );
     circleTwo.id = "circleTwo";  
-    circleTwo.className = "circle";    
+    circleTwo.className = "circle"; 
+
+    var circleTwoTitle = document.createElement( 'h1' );
+    circleTwoTitle.innerText = "Lava Tour";
+    circleTwo.appendChild( circleTwoTitle );
+
+    var circleTwoText = document.createElement( 'a' );
+    circleTwoText.innerText = "One of the sights we are really hoping to get to is a tour of the volcanos. \n\n If you'd like to help with this then give this a click."
+
+    circleTwo.onmouseover = function() {
+      circleTwo.innerText = "";
+      circleTwo.appendChild( circleTwoText );
+      circleTwoText.href = "mailto:euanandlaura@gmail.com";
+    }   
+
+    circleTwo.onmouseleave = function() {
+      circleTwo.innerText = "";
+      circleTwo.appendChild( circleTwoTitle );
+    }
 
     var circleThree = document.createElement( 'td' );
     circleThree.id = "circleThree";
-    circleThree.className = "circle";    
+    circleThree.className = "circle";
+
+    var circleThreeTitle = document.createElement( 'h1' );
+    circleThreeTitle.innerText = "Lava Tour";
+    circleThree.appendChild( circleThreeTitle );
+
+    var circleThreeText = document.createElement( 'a' );
+    circleThreeText.innerText = "Another of the main attractions we would love to get to is a boat tour of the Napali Coast. \n\n If you'd like to help with this then give this a click."
+
+    circleThree.onmouseover = function() {
+      circleThree.innerText = "";
+      circleThree.appendChild( circleThreeText );
+      circleThreeText.href = "mailto:euanandlaura@gmail.com";
+    }   
+
+    circleThree.onmouseleave = function() {
+      circleThree.innerText = "";
+      circleThree.appendChild( circleThreeTitle );
+    }    
 
     var circleFour = document.createElement( 'td' );
     circleFour.id = "circleFour"; 
-    circleFour.className = "circle";    
+    circleFour.className = "circle";  
+
+    var circleFourTitle = document.createElement( 'h1' );
+    circleFourTitle.innerText = "Hotels";
+    circleFour.appendChild( circleFourTitle );
+
+    var circleFourText = document.createElement( 'a' );
+    circleFourText.innerText = "Like the flights these are pretty essential. We are \n doing a fair amount of travelling throughout the islands so any help will be very much appreciated. \n\n If you'd like to help with these then give this a click."
+
+    circleFour.onmouseover = function() {
+      circleFour.innerText = "";
+      circleFour.appendChild( circleFourText );
+      circleFourText.href = "mailto:euanandlaura@gmail.com";
+    }   
+
+    circleFour.onmouseleave = function() {
+      circleFour.innerText = "";
+      circleFour.appendChild( circleFourTitle );
+    }    
 
     var circleFive = document.createElement( 'td' );
     circleFive.id = "circleFive";   
