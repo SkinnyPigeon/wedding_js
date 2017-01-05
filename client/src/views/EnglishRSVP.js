d9b635dabfbc01a63cf01c54366da8ce6197b6bf
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2017-01-05 23:25:53 +0000

    about to test it for the first time

--- client/src/views/EnglishRSVP.js
+++ client/src/views/EnglishRSVP.js
@@ -20,6 +20,12 @@ EnglishRSVP.prototype = {
     var comingBox = document.createElement( 'input' );
     comingBox.type = "checkbox";
 
+    var plusOneText = document.createElement( 'h4' );
+    plusOneText.innerText = "Plus One?";
+
+    var plusOneBox = document.createElement( 'input' );
+    plusOneBox.type = "checkbox";
+
     var kidsText = document.createElement( 'h4' );
     kidsText.innerText = "Are you taking any kids?";
 
@@ -32,22 +38,58 @@ EnglishRSVP.prototype = {
     kidsSlider.value = 0;
     kidsSlider.list = "steplist";
 
+
     var button = document.createElement( 'button' );
     button.innerText = "Go";
     button.style.padding = "1em";
     button.onclick = function() {
-      this.sendConfirmation();
-    }
 
+      var total = 0;
+      if( comingBox.checked && !plusOneBox.checked ) {
+        total = 1;
+      }
+      if( comingBox.checked && plusOneBox.checked ) {
+        total = 2;
+      }
+      if( !comingBox.checked && plusOneBox.checked ) {
+        total = 1;
+      }
+
+      var comingBoolean = false;
+      if( total >= 1 ) {
+        comingBoolean = true;
+      }
+
+      var request = new XMLHttpRequest()
+      request.open( 'POST', this.url )
+      request.setRequestHeader("Content-Type", "application/json")
+      request.onload = () => {
+        this.getComments();
+      }
+      var data = {
+        guest: {
+          name: name.value,
+          email: email.value,
+          adults: total,
+          kids: kidsSlider.value,
+          coming: comingBoolean
+        }
+      }
+      request.send( JSON.stringify( data ));
+    }.bind( this )
 
     rsvpSpace.appendChild( name );
     rsvpSpace.appendChild( email );
     rsvpSpace.appendChild( comingText );
     rsvpSpace.appendChild( comingBox );
+    rsvpSpace.appendChild( plusOneText );
+    rsvpSpace.appendChild( plusOneBox );
     rsvpSpace.appendChild( kidsText );
     rsvpSpace.appendChild( kidsSlider );
     rsvpSpace.appendChild( button );
   },
+
+
 
 }
 
